860c648f6c45ae5e753c05414e7ece140528d33c
Mayank <immayankvrm>
2020-06-04 07:02:36 +0530

    multi dice rolling

--- dice_roller.py
+++ dice_roller.py
@@ -1,11 +1,20 @@
 def main():
     import random
     dice_sum = 0
-    for i in range(0,2):
+    dice_size = int(input('how many sides are the dice'))
+    dice_roll = int(input('size of the dice'))
+    for i in range(0,dice_roll):
         roll = random.randint(1,6)
         dice_sum += roll
-        print(f'You rolled {roll}')
-    print(f'you have rolled total of {dice_sum}')    
+        if roll == 1:
+            print(f'You rolled {roll}' 'critical fail')
+        elif roll == dice_size :
+            print(f'You rolled {roll}' 'critical success')
+
+        else:
+            (f'you rolled {roll}')
+
+    print(f'you have rolled total of {dice_sum}')
 
 if __name__== "__main__":
   main()
